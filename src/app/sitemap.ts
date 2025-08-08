fe7702008fba5d67c4bbd11b7e132169ba0063f5
HTG HUZAIFA <htghuzaifa2@gmail.com>
2025-08-08 12:48:33 +0000

    fix all errros make it ready to deploy on cloudflare pages

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,13 +1,9 @@
 import { MetadataRoute } from 'next';
-import { headers } from 'next/headers';
 
 export const dynamic = 'force-static';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const heads = headers();
-  const proto = heads.get('x-forwarded-proto');
-  const host = heads.get('host');
-  const url = `${proto}://${host}`;
+  const url = 'https://webp.huzi.pk';
 
   return [
     {
